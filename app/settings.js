525aea632ae1d5c2c6c9cdd1c35a2e5dedae452e
art-coral <f.l.demchenko@gmail.com>
2014-02-10 00:50:13 +0200

    Create settings.js

---
+++ app/settings.js
@@ -0,0 +1,13 @@
+drupalgap.settings = {
+  'site_path':'http://mob.art-coral.com', /* e.g. http://www.example.com */
+  'base_path':'/',
+  'language':'und',
+  'file_public_path':'sites/default/files',
+  'debug':true, /* set to true to see console.log debug information */
+  'front':'dashboard.html',
+  'offline':'offline.html',
+  'clean_urls':false, /* set to true if you have clean urls enabled on your site */
+};
+drupalgap.modules.custom = [
+  /*{'name':'my_module'},*/
+];
